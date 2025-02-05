470ab4d305c7844033b7325309078826a504123c
Ivan-napylov <napylovivan03@gmail.com>
2025-02-06 03:35:04 +0500

    commit

---
+++ Lab3/Task 1/1.4 Variables/script.js
@@ -0,0 +1,19 @@
+let admin;
+let name;
+
+name = "John";
+
+admin = name;
+
+alert(admin);
+
+
+
+let planetName = "Mars";
+let currentUserName = "John";
+
+
+
+
+const BIRTHDAY = '18.04.1982';
+const age = someCode(birthday);
---
+++ Lab3/Task 2/script.js
@@ -0,0 +1,40 @@
+let task = [];
+
+
+
+function addTask() {
+    let taskText = document.getElementById("taskInput").value;
+    if (taskText.trim() === "") return;
+
+    let taskList = document.getElementById("taskList");
+    let li = document.createElement("li");
+    li.className = "list-group-item";
+    
+    let checkbox = document.createElement("input");
+    checkbox.type = "checkbox";
+    checkbox.className = "form-check-input";
+    checkbox.onclick = function() {
+        if (checkbox.checked) {
+            taskTextSpan.style.textDecoration = "line-through";
+        } else {
+            taskTextSpan.style.textDecoration = "none";
+        }
+    };
+    
+    let taskTextSpan = document.createElement("span");
+    taskTextSpan.textContent = taskText;
+    
+    let deleteBtn = document.createElement("button");
+    deleteBtn.className = "btn-danger";
+    deleteBtn.innerHTML = "&#128465;";
+    deleteBtn.onclick = function() {
+        taskList.removeChild(li);
+    };
+    
+    li.appendChild(checkbox);
+    li.appendChild(taskTextSpan);
+    li.appendChild(deleteBtn);
+    taskList.appendChild(li);
+    
+    document.getElementById("taskInput").value = "";
+}
